634e4ab7c91dd8c3ccb54e742e0a292c022ef085
thanhbinh9412002 <nguyenthanhbinhntb9@gmail.com>
2024-05-21 07:44:28 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import React from "react"
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+//import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
 import './App.css'
 import AddRoom from './components/room/AddRoom'
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/room/AddRoom.jsx
+++ src/components/room/AddRoom.jsx
@@ -3,7 +3,7 @@ import { addRoom } from '../utils/ApiFunctions'
 import RoomTypeSelector from '../common/RoomTypeSelector'
 
 const AddRoom = () => {
-  const[newRome, setNewRoom] = useState({
+  const[newRoom, setNewRoom] = useState({
     photo : null,
     roomType : "",
     roomPrice : ""
@@ -16,7 +16,7 @@ const AddRoom = () => {
   const handleRoomInputChange = (e) =>{
     const name = e.target.name
     let value = e.target.value
-    if (name == "roomPrice"){
+    if (name === "roomPrice"){
       if(!isNaN(value))
         {
           value.parseInt(value)
@@ -31,7 +31,7 @@ const AddRoom = () => {
 
   const handleImageChange = (e) =>{
     const selectImage = e.target.files[0]
-    setNewRoom({...newRoom, photo: selectImage})
+    setNewRoom({ ...newRoom, photo: selectImage })
     setImagePreview(URL.createObjectURL(selectImage))
   }
 
@@ -40,9 +40,9 @@ const AddRoom = () => {
     e.preventDefault()
     try{
       const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
-      if(success != indefined)
+      if(success != undefined)
         {
-          setSuccessMessage("A newroom was added to the database")
+          setSuccessMessage("A new room was added to the database")
           setNewRoom({photo:null, roomType:"", roomPrice:""})
           setImagePreview("")
           setErrorMessage("")
@@ -59,12 +59,12 @@ const AddRoom = () => {
 
   return (
     <>
-    <section className='container, mt-5 mb-5'>
-      <div className='row justify-content-center'>
-        <div className='col-md-8 col-lg-6'>
-          <h2 className='mt-5 mb-2'>Add a new room</h2>
+    <section className="container, mt-5 mb-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6">
+          <h2 className="mt-5 mb-2">Add a new room</h2>
           <form onSubmit={handleSubmit}></form>
-            <div className='mb-3'>
+            <div className="mb-3">
               <label htmlFor='roomType' className="form-lable">Room Type</label>
               <div>
                   <RoomTypeSelector handleRoomInputChange={handleRoomInputChange}
@@ -73,21 +73,21 @@ const AddRoom = () => {
               </div>
             </div>
 
-            <div className='mb-3'>
+            <div className="mb-3">
               <label htmlFor='roomPrice' className="form-lable">Room Price</label>
-              <input className='form-control'
+              <input className="form-control"
                       required
                       id='roomPrice'
-                      type='number'
+                      type="number"
                       name='roomPrice'
                       value={newRoom.roomPrice}
                       onChange={handleRoomInputChange}
               />
             </div>
 
-            <div className='mb-3'>
+            <div className="mb-3">
               <label htmlFor='photo' className="form-lable">Room Photo</label>
-              <input  className='form-control'
+              <input  className="form-control"
                       id='photo'
                       name='photo'
                       type='file'
@@ -96,12 +96,12 @@ const AddRoom = () => {
               {imagePreview && (
                 <img src={imagePreview} alt="Preview Room Photo"
                 style={{maxWidth:"400px", maxHeight:"400px"}}
-                className='mb-3'/>
+                className="mb-3"/>
               )}
             </div>
 
-            <div className='d-grid d-md-flex mt-2'>
-              <button className='btn btn-outline-primary ml-5'> Save Room</button>
+            <div className="d-grid d-md-flex mt-2">
+              <button className="btn btn-outline-primary ml-5"> Save Room</button>
             </div>
         </div>
       </div>
